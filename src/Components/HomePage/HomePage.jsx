88cf1ce201b0cc8495c7c7b36c02514c521c8f59
kyow <chakkek15@gmail.com>
2024-05-10 19:43:02 +0600

    added footer component

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar/NavBar'
 
 function HomePage() {
   return (
     <>
-    
+      
     </>
   )
 }
--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./HomePage/NavBar/NavBar"
+import Footer from "./HomePage/Footer/Footer"
 
 function Root() {
   return (
    <>
    <NavBar></NavBar>
    <Outlet></Outlet>
+   <Footer></Footer>
    </>
   )
 }
